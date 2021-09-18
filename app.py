1aefcff1d5a942d39f248a24e1a92671b25c27a6
Luca Fontana <lucafont88@gmail.com>
2021-09-18 16:45:24 +0200

    wip typings

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+from libs.plotting.plot_model import PlotModel
+from typing import Any, Dict
+from scipy.stats.stats import CumfreqResult, DescribeResult, RelfreqResult
+from sklearn.pipeline import Pipeline
 from finance_mining_service import FinanceMiningService
 import streamlit as st
 
@@ -5,6 +9,12 @@ st.title('FinFetch Analyzer')
 
 PROMPT_DEBUG = False
 finance_mining_service = FinanceMiningService(PROMPT_DEBUG)
+stats: DescribeResult = None
+rel_freq_result: RelfreqResult = None
+cum_freq_result: CumfreqResult = None
+model_info: Dict[str, Any] = {}
+poly_models: Dict[int, Pipeline] = {}
+plot_model: PlotModel = None
 stats, rel_freq_result, cum_freq_result, model_info, poly_models, plot_model = finance_mining_service.run()
 
 # st.write(stats)
